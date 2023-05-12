ffa17552b16f65ae5de75ddc65ea0e4297c804eb
ubuntonaresh <ubuntonaresh@gmail.com>
2023-05-12 17:47:30 +0530

    Chnages for host

--- CodingWithMitchChat/wsgi.py
+++ CodingWithMitchChat/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatServerPlayground.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CodingWithMitchChat.settings')
 
 application = get_wsgi_application()
