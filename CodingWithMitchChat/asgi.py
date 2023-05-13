1bac503a9c1bf9af391428c7821a29db6f967054
ubuntonaresh <ubuntonaresh@gmail.com>
2023-05-13 13:21:52 +0530

    S

--- CodingWithMitchChat/asgi.py
+++ CodingWithMitchChat/asgi.py
@@ -8,6 +8,6 @@ import django
 from decouple import config
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", f'{config("PROJECT_NAME")}.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'CodingWithMitchChat.settings')
 django.setup()
 application = get_default_application()
